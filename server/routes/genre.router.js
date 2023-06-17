546a4d9e05571bc6a6ebfd9a53841ca0da4c84cc
Dave Nash <davidjnash2@gmail.com>
2023-06-16 23:34:16 -0500

    finished building genresGET in genrerouterjs

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  // query to get all genres
+  const query = `SELECT * FROM "genres";`
+  pool.query(query)
+    .then((response) => {
+      console.log('response on server genres GET is:', response.rows);
+      res.send(response.rows)
+    })
+    .catch((error) => {
+      console.log('error in server genres GET');
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
