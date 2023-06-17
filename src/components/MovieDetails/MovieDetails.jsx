b620bac6bae9acec5a4143d423f0d5ccbdfe5724
Dave Nash <davidjnash2@gmail.com>
2023-06-17 12:43:05 -0500

    created MovieDetailscss to change text alignment; finished MovieDetails component for base by building out render field to show on DOM using returned data from server, and added conditional render to allow user to navigate back to home for when details state returns to null on refresh, until I can get state to remain after refresh

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,31 +1,43 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
-
-
+import './MovieDetails.css';
 
 
 function MovieDetails() {
+    // bring in specific movie item details from global state
     const details = useSelector(store => store.details);
-    // const dispatch = useDispatch();
+
+    // bring in history to allow redirecting view
     const history = useHistory();
 
-    // useEffect(() => {
-    //     dispatch({ type: 'FETCH_DETAILS' });
-    //     // make get for specific movie details
-    // }, []);
+    // since haven't yet figured out how to keep data on page after 
+    // refresh, adding this conditional render so that code doesn't 
+    // break, which will allow user to click button to 
+    // return to home page to make another selection
+    if (details.length === 0) {
+        return <div>
+            OOOOOPS...nothing here.
+            <br />
+            <br />
+            <br />
+            <button onClick={() => history.push('/')}>TAKE ME BACK TO THE LIST</button>
+        </div>;
+    }
 
-
+    // rendering returned movie detail data, using conditional rendering to
+    // determine genre/genres heading
     return (
         <div>
-            <h1>{details.title}</h1>
-            <img src={details.poster} alt={details.title} />
-            <p>Genre:{details.genres}</p>
-            <p>Description:{details.description}</p>
+            <h1>{details[0].title}</h1>
+            <img src={details[0].poster} alt={details[0].title} />
+            {details[0].json_agg.length > 1 ?
+                <h3>Genres:</h3> : <h3>Genre:</h3>}
+            <p>{details[0].json_agg.join(', ')}</p>
+            <h3>Description:</h3>
+            <p>{details[0].description}</p>
             <button onClick={() => history.push('/')}>TAKE ME BACK TO THE LIST</button>
         </div>
-
     );
 }
 
